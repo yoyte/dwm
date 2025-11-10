1e853f436b7c74e71c9aeae52ba0b749489114e2
yoyte <141865560+yoyte@users.noreply.github.com>
2025-11-10 17:19:59 +0200

    Update config.h

--- config.h
+++ config.h
@@ -47,6 +47,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const int taglayouts[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 static const Rule rules[] = {
 	/* xprop(1):
